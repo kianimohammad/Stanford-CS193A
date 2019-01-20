ab387950676aa58856c58df7bd884b673d2815a3
siddharth2010 <ssiddharth2010@gmail.com>
2019-01-20 12:43:51 +0530

    Complete color Switching

--- TMNTApp/app/src/main/java/com/example/tmntapp/MainActivity.java
+++ TMNTApp/app/src/main/java/com/example/tmntapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.tmntapp;
 
+import android.graphics.Color;
+import android.support.annotation.ColorInt;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +16,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void changeColor(View view) {
-        
+        LinearLayout ll = (LinearLayout) findViewById(R.id.layout);
+
+        if (view.getId() == R.id.radio_red) {
+            ll.setBackgroundColor(Color.RED);
+        } else if(view.getId() == R.id.radio_blue) {
+            ll.setBackgroundColor(Color.BLUE);
+        } else {
+            ll.setBackgroundColor(Color.GREEN);
+        }
     }
 }
 
