ef359dbc74600ac58e0bb20e304c6815a74f88b2
siddharth2010 <ssiddharth2010@gmail.com>
2019-01-20 14:25:46 +0530

    Set up the dynamic list

--- DictionaryApp/app/src/main/java/com/example/dictionaryapp/MainActivity.java
+++ DictionaryApp/app/src/main/java/com/example/dictionaryapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.dictionaryapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String [] dynamic = {"OK!", "This", "is", "Epic!"}
+
+
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<String>(this,
+                        android.R.layout.simple_list_item_1,
+                        dynamic);
+
+        ListView list = (ListView) findViewById(R.id.dynamic_list);
+        list.setAdapter(adapter);
     }
 }
