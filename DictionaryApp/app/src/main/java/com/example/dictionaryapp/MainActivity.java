83bd7dca128d30bfab4616a3cd6ef2e225c0a1c7
siddharth2010 <ssiddharth2010@gmail.com>
2019-01-20 14:43:40 +0530

    Set up onClick Listener for Dynamic List

--- DictionaryApp/app/src/main/java/com/example/dictionaryapp/MainActivity.java
+++ DictionaryApp/app/src/main/java/com/example/dictionaryapp/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.dictionaryapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,5 +26,24 @@ public class MainActivity extends AppCompatActivity {
 
         ListView list = (ListView) findViewById(R.id.dynamic_list);
         list.setAdapter(adapter);
+
+        // set up onclick for the dynamic list (cannot be done via XML integration)
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            // Using an anonymous inner class as we are not allowed to pass methods in Java
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // the code for the listener function
+                MakeToast("the user clicked item " + position);
+            }
+        });
+
     }
+
+    public void MakeToast(String text) {
+        Toast.makeText(this, "The Color is now: " + text, Toast.LENGTH_LONG).show();
+        Log.d("Toast", text + " toast now on screen");
+    }
+    
+
 }
