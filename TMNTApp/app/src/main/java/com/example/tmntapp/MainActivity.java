5cb8beadf6939dddba0a2dde38686a05e39c3816
siddharth2010 <ssiddharth2010@gmail.com>
2019-01-20 12:48:36 +0530

    Add Logging and Toast support

--- TMNTApp/app/src/main/java/com/example/tmntapp/MainActivity.java
+++ TMNTApp/app/src/main/java/com/example/tmntapp/MainActivity.java
@@ -4,8 +4,10 @@ import android.graphics.Color;
 import android.support.annotation.ColorInt;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,14 +19,26 @@ public class MainActivity extends AppCompatActivity {
 
     public void changeColor(View view) {
         LinearLayout ll = (LinearLayout) findViewById(R.id.layout);
+        String color;
 
         if (view.getId() == R.id.radio_red) {
             ll.setBackgroundColor(Color.RED);
+            color = "Red";
         } else if(view.getId() == R.id.radio_blue) {
             ll.setBackgroundColor(Color.BLUE);
+            color = "Blue";
         } else {
             ll.setBackgroundColor(Color.GREEN);
+            color = "Green";
         }
+        Log.d("Color Switch", "Changed view");
+        MakeToast(color);
+
+    }
+
+    public void MakeToast(String text) {
+        Toast.makeText(this, "The Color is now: " + text, Toast.LENGTH_LONG).show();
+        Log.d("Toast", text + " toast now on screen");
     }
 }
 
