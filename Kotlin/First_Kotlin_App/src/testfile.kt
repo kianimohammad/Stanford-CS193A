469a018f94c08028adb26451f9ca726be17ed0cd
siddharth2010 <ssiddharth2010@gmail.com>
2019-01-29 20:22:51 +0530

    Kotlin Hello, World!

---
+++ Kotlin/First_Kotlin_App/src/testfile.kt
@@ -0,0 +1,29 @@
+package demo
+
+fun main( args: Array<String>) {
+    println("Hello, World!")
+
+
+// val is for immutable, while var is for mutable
+
+    val name = "Siddharth"
+    var age = 21
+
+    var bigInt = Int.MAX_VALUE
+    var smallInt = Int.MIN_VALUE
+
+    println("Largest : $bigInt")
+    println("Smallest : " + smallInt)
+
+
+
+
+
+
+
+
+
+
+
+
+}
